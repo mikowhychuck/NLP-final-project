4b565dda31a13164590275992f072ab804ddcf78
Adam Mikolajczak <adam.mikolajczak111@gmail.com>
2025-01-09 16:51:04 +0100

    images of dogs are now visible after classyfication

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from bert_classificator import get_breed
+from bert_classificator import get_breed, get_eng_name
 
 app = Flask(__name__)
 
@@ -12,7 +12,8 @@ def classify_text():
     data = request.json
     text = data.get('text', '')
     result = get_breed(text)
-    return jsonify({'class': result})
+    eng_name = get_eng_name(result)
+    return jsonify({'class': result, 'eng_name': eng_name})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/bert_classificator.py
+++ src/bert_classificator.py
@@ -1,7 +1,32 @@
 from transformers import pipeline
+import json
+
+dog_breeds = {
+    "labrador retriever": "retriever",
+    "yorkshire terrier": "terrier/yorkshire",
+    "owczarek niemiecki": "germanshepherd",
+    "beagle": "beagle",
+    "golden retriever": "retriever/golden",
+    "buldog": "bulldog",
+    "mops": "pug",
+    "cocker spaniel": "spaniel",
+    "shih tzu": "shihtzu",
+    "border collie": "collie",
+    "maltańczyk": "maltese",
+    "rottweiler": "rottweiler",
+    "doberman": "doberman",
+    "akita inu": "akita",
+    "husky": "husky",
+    "chihuahua": "chihuahua"
+}
 
 def get_breed(text):
     classifier = pipeline("text-classification", model="dog_breed_classifier", tokenizer="dog_breed_classifier")
     result = classifier(text)
     result = result[0]['label']
+    result = result.replace('-', ' ')
+    eng_name = dog_breeds[result]
     return result
+
+def get_eng_name(text):
+    return dog_breeds[text]
